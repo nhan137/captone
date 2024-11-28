0259791fc7e079795ab698f379d1a4f4bbe4e18b
PhanPhuTuan <phutuan112@gmail.com>
2024-11-29 04:11:49 +0700

    update sidebar accountant

--- hash_password.php
+++ hash_password.php
@@ -1,6 +1,6 @@
 <?php
 // Nhập mật khẩu bạn muốn mã hóa
-$password = 'nhannguyen';
+$password = 'admin';
 
 // Mã hóa mật khẩu
 $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
@@ -8,4 +8,4 @@ $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
 // In ra mật khẩu đã mã hóa
 echo "Mật khẩu đã mã hóa: " . $hashedPassword;
 
-                
+//admin - $2y$10$DHZ0d9wRTGt1WZoHEpCdGuIYo3drSft9uu4Qsjm/ZHQ/k9poLXLFy    
